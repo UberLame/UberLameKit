adef3a0978eb04f2609ef1aa7fa206fd027117f5
UberLame <w0rm@sploit.me.uk>
2013-11-24 11:39:31 -0600

    functional

--- master.py
+++ master.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python
 
 import optparse as theparser
-from lib.Zombie import *
+from lib.zHash import *
 
 def main():
 	parser = theparser.OptionParser("%prog stuff")
-	parser.add_option('-L', dest='myFile', type='string', help='bot file')
+	parser.add_option('-E', dest='myEngi', type='string', help='please see README')
+	parser.add_option('-D', dest='myDict', type='string', help='dictionary file')
+	parser.add_option('-T', dest='myTarg', type='string', help='the target file for input')
 	(options, args) = parser.parse_args()
-	myFile = options.myFile
 
-	x = Zombie().woot(myFile)
+	myEngi = options.myEngi
+	myDict = options.myDict
+	myTarg = options.myTarg
+
+	if (myEngi == 'unhash'):
+		x = Unhash().checkMe(myTarg, myDict, 'auto')
+
 
 if __name__ == '__main__':
 	main()
