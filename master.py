264583f81f4620c70907b2e469786c76736d33e9
UberLame <w0rm@sploit.me.uk>
2013-11-25 14:07:18 -0600

    fixed

--- master.py
+++ master.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python
 
 import optparse as theparser
-from lib.checkhash import *
+from eng.checkhash import *
+from eng.checkbrute import *
 
 def main():
 	parser = theparser.OptionParser("%prog stuff")
 	parser.add_option('-E', dest='myEngi', type='string', help='please see README')
 	parser.add_option('-D', dest='myDict', type='string', help='dictionary file')
 	parser.add_option('-T', dest='myTarg', type='string', help='the target file for input')
+	parser.add_option('-U', dest='myUser', type='string', help='the target username')
+	parser.add_option('-H', dest='myHost', type='string', help='the target hostname or ipaddress')
+	parser.add_option('-A', dest='myType', type='string', help='the attack type for the engine \(if required\)')
 	(options, args) = parser.parse_args()
 
 	myEngi = options.myEngi
 	myDict = options.myDict
 	myTarg = options.myTarg
+	myUser = options.myUser
+	myHost = options.myHost
+	myType = options.myType
 
 	if (myEngi == 'unhash'):
 		x = Unhash().checkMe(myTarg, myDict)
+	elif (myEngi == 'bruteforce'):
+		x = Bruteforce().checkMe(myHost, myUser, myDict, myType)
+	else:
+		print "[E] please read the README file for more help"
 
 
 if __name__ == '__main__':
